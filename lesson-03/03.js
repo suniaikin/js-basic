7eda398098065fd9a4b33848890149a7c20ebed9
suniaikin <class.suniaikin@gmail.com>
2025-08-16 17:10:30 -0700

    03-03 task

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,16 @@
-// Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
+// Разработайте функцию findLargest, которая принимает три числа
+// и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(x, y, z) {
+	if (x > y && x > z) {
+		return x;
+	} else if ( y > x && y > z) {
+		return y;
+	} else return z;
+}
+
+let out = findLargest(11, 5, 10)
+console.log(out)
+
+
+
