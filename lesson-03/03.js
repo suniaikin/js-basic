a96db37fec18a8a9f2d73c04982666d5368e5240
suniaikin <class.suniaikin@gmail.com>
2025-08-16 17:33:47 -0700

    03-03

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -4,7 +4,7 @@
 function findLargest(x, y, z) {
 	if (x > y && x > z) {
 		return x;
-	} else if ( y > x && y > z) {
+	} else if (y > x && y > z) {
 		return y;
 	} else return z;
 }
@@ -12,5 +12,12 @@ function findLargest(x, y, z) {
 let out = findLargest(11, 5, 10)
 console.log(out)
 
+function findLargest2(x, y, z) {
+	return (x > y && x > z)
+		? x : (y > x && y > z)
+		? y : z
+}
 
+let opt2 = findLargest2(21, 5, 10)
+console.log(opt2)
 
