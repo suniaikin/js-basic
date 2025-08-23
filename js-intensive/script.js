dcb933c75583e29ec52c07dc2eb26b68981b9b78
suniaikin <class.suniaikin@gmail.com>
2025-08-22 18:47:28 -0700

    step2

--- js-intensive/script.js
+++ js-intensive/script.js
@@ -4,3 +4,13 @@
  
         // Добавление элемента заголовка в тело документа
         document.body.append(titleElement);
+        
+        // Создание выпадающего списка (select)
+        const techSelectElement = document.createElement('select');
+ 
+        // Создание первого варианта (option) для выпадающего списка
+        const techSelectOption0Element = document.createElement('option');
+        techSelectOption0Element.value = 1;
+        techSelectOption0Element.append('JS'); // Добавление текста 'JS' к первому варианту
+        techSelectElement.append(techSelectOption0Element); // Добавление первого варианта в выпадающий список
+ 
