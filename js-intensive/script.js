57f89c9f9392f877845bdf561471760c439ca998
suniaikin <class.suniaikin@gmail.com>
2025-08-22 18:46:37 -0700

    step1

--- js-intensive/script.js
+++ js-intensive/script.js
@@ -1,3 +1,6 @@
-    <script>
-        alert('hello from it-incubator');
-    </script>
+// Создание элемента заголовка (h1) и добавление текста к этому элементу
+        const titleElement = document.createElement('h1');
+        titleElement.append('Hello from it-incubator');
+ 
+        // Добавление элемента заголовка в тело документа
+        document.body.append(titleElement);
