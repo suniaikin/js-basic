ba203514a878a0cd45f4391751510557662cadb9
suniaikin <class.suniaikin@gmail.com>
2025-08-22 18:49:13 -0700

    step4

--- js-intensive/script.js
+++ js-intensive/script.js
@@ -22,3 +22,19 @@
  
         // Добавление второго варианта в тело документа (вместо этого, вероятно, предполагалось добавить сам выпадающий список)
         document.body.append(techSelectElement);
+        
+        // data
+const title = 'it-incubator';
+const graduatesCount = 2000;
+const areYouChampion = true;
+// render
+let pageTitleElement = document.createElement('h1');
+pageTitleElement.innerText = title;
+document.body.append(pageTitleElement);
+let graduatesCountElement = document.createElement('input');
+graduatesCountElement.value = graduatesCount;
+document.body.append(graduatesCountElement);
+let areYouChampionElement = document.createElement('input');
+areYouChampionElement.type = 'checkbox';
+areYouChampionElement.checked = areYouChampion;
+document.body.append(areYouChampionElement);
